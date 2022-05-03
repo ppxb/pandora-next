01a24d8739e4cbb69f6c3eda7bb5efacf062452e
ppxb <ppxb123@gmail.com>
2022-05-03 23:38:09 +0800

    chore: index page ui

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,91 +1,23 @@
 <template>
   <div class="index">
+    <div class="index-slot"></div>
     <div class="index-content">
       <div class="index-title">PANDORA</div>
-      <div class="index-version">ALPHA V0.0.1</div>
-      <button class="index-start">Get Start</button>
+      <div class="index-version"># VERSION 0.0.1</div>
+      <button class="index-start" @click="start">SIGN IN</button>
     </div>
-  </div>
-  <div>
-    <h1>Welcome to Pandora World!</h1>
-    <template v-if="!loadPlayer.name">
-      <h2>Please create your player!</h2>
-      <h3>Type your player's name</h3>
-      <input type="text" v-model="player.name" />
-      <div>
-        <h3>Choose your player's role</h3>
-        <select v-model="player.role">
-          <option v-for="role in roleDataTable" :key="role.id" :value="role">
-            {{ role.name }}
-          </option>
-        </select>
-      </div>
-      <div>
-        <h3>Choose your player's race</h3>
-        <select v-model="player.race">
-          <option v-for="race in raceDataTable" :key="race.id" :value="race">
-            {{ race.name }}
-          </option>
-        </select>
-        <p>Talent：</p>
-        <div v-for="talent in player.race.talents" :key="talent.id">
-          <p>{{ getData(talent).name }} - {{ getData(talent).desc }}</p>
-        </div>
-      </div>
-      <button @click="create">Create</button>
-    </template>
-    <template v-else>
-      <h2>Please select your active player</h2>
-      <h3>Player</h3>
-      <div style="border: 1px dashed #333; width: 400px; padding: 20px">
-        <p>name：{{ loadPlayer && loadPlayer.name }}</p>
-        <p>level：{{ loadPlayer.level && loadPlayer.level.current }}</p>
-        <p>role：{{ loadPlayer.role && loadPlayer.role.name }}</p>
-        <p>race：{{ loadPlayer.race && loadPlayer.race.name }}</p>
-        <button @click="select">select</button>
-      </div>
-    </template>
+    <footer class="index-footer">
+      <a href="https://github.com/ppxb" target="_blank">BUILT BY @PPXB</a>
+    </footer>
   </div>
 </template>
 
 <script setup>
-import { onMounted, reactive } from 'vue'
-import createNewPlayer from '~/core/create-player'
-import raceDataTable from '~/core/data/race-data'
-import roleDataTable from '~/core/data/role-data'
-import getData from '~/core/get-data'
-import { save, load } from '~/core/save-load'
-
-import { usePlayerStore } from '~/store/player'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
-const player = reactive({
-  name: '',
-  role: getData(900001),
-  race: getData(1000002)
-})
-
-const loadPlayer = $ref({})
-
-const store = usePlayerStore()
-
-onMounted(() => {
-  loadPlayer = load()
-})
-
-const create = () => {
-  if (!player.name) return
-  const newPlayer = createNewPlayer(player.name, player.role.id, player.race.id)
-  player.name = ''
-  store.state = newPlayer
-  save(store.state)
-  router.push('/lobby')
-}
-
-const select = () => {
-  store.state = loadPlayer
+const start = () => {
   router.push('/lobby')
 }
 </script>
@@ -95,42 +27,60 @@ const select = () => {
   display: flex;
   align-items: center;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
+  padding: 2em;
   background: url('~/assets/images/index_cover.png') bottom/cover no-repeat;
 }
 
+.index-slot {
+  flex-shrink: 1;
+}
+
 .index-content {
-  width: fit-content;
   display: flex;
   flex-direction: column;
+  align-items: center;
 }
 
 .index-title {
-  color: #fefefe;
-  font-size: 8em;
+  color: var(--primary);
+  font-size: 10em;
   line-height: 0.625em;
   font-weight: 700;
 }
 
 .index-version {
-  color: rgba(255, 255, 255, 0.7);
+  color: var(--primary);
   font-size: 1.5em;
   align-self: flex-end;
-  margin-bottom: 2em;
 }
 
 .index-start {
-  border: 2px solid #fefefe;
+  margin-top: 4em;
+  border: 2px solid var(--primary);
   font-size: 1.5em;
   background-color: transparent;
-  color: #fefefe;
-  padding: 12px 4em;
+  color: var(--primary);
+  padding: 0.5em 4em;
   transition: all 0.25s ease;
 }
 
 .index-start:hover {
-  background-color: #fefefe;
-  color: #222;
+  background-color: var(--primary);
+  color: var(--main-text);
   cursor: pointer;
+}
+
+.index-footer {
+  flex-shrink: 1;
+  font-size: 1.25em;
+}
+
+.index-footer a {
+  color: rgba(249, 170, 0, 0.8);
+  transition: all 0.25s ease;
+}
+.index-footer a:hover {
+  color: var(--primary);
 }
 </style>
--- src/pages/lobby.vue
+++ src/pages/lobby.vue
@@ -1,242 +1,85 @@
 <template>
-  <div class="lobby">
-    <div>
-      <h1>Player Status：</h1>
-      <div style="display: flex; flex-wrap: wrap">
-        <div
-          style="
-            border: 1px dashed #333;
-            width: fit-content;
-            padding: 10px 20px;
-            margin: 20px;
-          "
-        >
-          <h2>Info</h2>
-          <h3>name：{{ player.name }}</h3>
-          <h3>level：{{ player.level.current }}</h3>
-          <h3>exp：{{ player.level.exp }} / {{ player.level.nextExp }}</h3>
-          <h3>role：{{ player.role.name }}</h3>
-          <h3>gil：{{ player.resource.gil }}</h3>
-        </div>
-
-        <div
-          style="
-            border: 1px dashed #333;
-            width: fit-content;
-            padding: 10px 20px;
-            margin: 20px;
-          "
-        >
-          <h2>Base</h2>
-          <h3 v-for="key in Object.keys(player.base)" key="key">
-            {{ key.substring(1) }}：{{ player.base[key] }}
-          </h3>
-        </div>
-
-        <div
-          style="
-            border: 1px dashed #333;
-            width: fit-content;
-            padding: 10px 20px;
-            margin: 20px;
-          "
-        >
-          <h2>Attack</h2>
-          <h3 v-for="key in Object.keys(player.attack)" key="key">
-            {{ key.substring(1) }}：{{
-              key != '$att' ? player.attack[key] + '%' : player.attack[key]
-            }}
-          </h3>
-        </div>
-
-        <div
-          style="
-            border: 1px dashed #333;
-            width: fit-content;
-            padding: 10px 20px;
-            margin: 20px;
-          "
-        >
-          <h2>Defense</h2>
-          <h3 v-for="key in Object.keys(player.defense)" key="key">
-            {{ key.substring(1) }}：{{
-              key != '$dodge'
-                ? player.defense[key]
-                : player.defense[key] * 100 + '%'
-            }}
-          </h3>
-        </div>
-
-        <div
-          style="
-            border: 1px dashed #333;
-            width: fit-content;
-            padding: 10px 20px;
-            margin: 20px;
-          "
-        >
-          <h2>Equipments</h2>
-          <h3
-            v-for="(item, index) in player.equipments.filter(item => item != 0)"
-            key="item"
-          >
-            <button @click="show(item)">
-              {{ index }} - {{ unpack(item).name }}
-            </button>
-          </h3>
-        </div>
-
-        <div
-          style="
-            border: 1px dashed #333;
-            width: fit-content;
-            padding: 10px 20px;
-            margin: 20px;
-          "
-        >
-          <h2>Skills</h2>
-          <h3>active</h3>
-          <h3
-            v-for="(item, index) in player.skills.active.filter(
-              item => item != 0
-            )"
-            key="item"
-          >
-            <button @click="show(item)">
-              {{ index }} - {{ unpack(item).name }}
-            </button>
-          </h3>
-          <h3>passive</h3>
-          <h3 v-for="(item, index) in player.skills.passive" key="item">
-            <button @click="show(item)">
-              {{ index }} - {{ unpack(item).name }}
-            </button>
-          </h3>
-        </div>
-
-        <div
-          style="
-            border: 1px dashed #333;
-            width: fit-content;
-            padding: 10px 20px;
-            margin: 20px;
-          "
-        >
-          <h1>Maps</h1>
-          <h3 v-for="(item, index) in mapDataTable" key="item">
-            <button @click="startCombat(item)">
-              {{ index }} - {{ unpack(item).name }}
-            </button>
-          </h3>
+  <div>
+    <h1>Welcome to Pandora World!</h1>
+    <template v-if="!loadPlayer.name">
+      <h2>Please create your player!</h2>
+      <h3>Type your player's name</h3>
+      <input type="text" v-model="player.name" />
+      <div>
+        <h3>Choose your player's role</h3>
+        <select v-model="player.role">
+          <option v-for="role in roleDataTable" :key="role.id" :value="role">
+            {{ role.name }}
+          </option>
+        </select>
+      </div>
+      <div>
+        <h3>Choose your player's race</h3>
+        <select v-model="player.race">
+          <option v-for="race in raceDataTable" :key="race.id" :value="race">
+            {{ race.name }}
+          </option>
+        </select>
+        <p>Talent：</p>
+        <div v-for="talent in player.race.talents" :key="talent.id">
+          <p>{{ getData(talent).name }} - {{ getData(talent).desc }}</p>
         </div>
       </div>
-    </div>
-    <div>
-      <h1>Combat log</h1>
-      <div class="combat-wrapper">
-        <div v-if="isCombat">
-          <div v-for="log in combatLog">
-            {{ log }}
-          </div>
-        </div>
-        <div v-if="isWait">
-          <div v-for="log in waitLog">
-            {{ log }}
-          </div>
-        </div>
+      <button @click="create">Create</button>
+    </template>
+    <template v-else>
+      <h2>Please select your active player</h2>
+      <h3>Player</h3>
+      <div style="border: 1px dashed #333; width: 400px; padding: 20px">
+        <p>name：{{ loadPlayer && loadPlayer.name }}</p>
+        <p>level：{{ loadPlayer.level && loadPlayer.level.current }}</p>
+        <p>role：{{ loadPlayer.role && loadPlayer.role.name }}</p>
+        <p>race：{{ loadPlayer.race && loadPlayer.race.name }}</p>
+        <button @click="select">select</button>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
 <script setup>
-import { usePlayerStore } from '../store/player'
-import { unpack } from '../core/utils'
-import computedPlayer from '../core/computed-player'
-import mapDataTable from '../core/data/map-data'
-import monsterDataTable from '../core/data/monster-data'
-import { combat } from '../core/combat'
-import { onUnmounted } from 'vue'
-import calcLoot from '~/core/calc-loot'
+import { onMounted, reactive } from 'vue'
+import createNewPlayer from '~/core/create-player'
+import raceDataTable from '~/core/data/race-data'
+import roleDataTable from '~/core/data/role-data'
+import getData from '~/core/get-data'
+import { save, load } from '~/core/save-load'
 
-const { state } = usePlayerStore()
+import { usePlayerStore } from '~/store/player'
+import { useRouter } from 'vue-router'
 
-const player = computedPlayer(state)
-const combatLog = $ref([])
-const tempLog = $ref([])
-const isCombat = $ref(false)
-const isWait = $ref(false)
-let combatTimer = null
-let waitTimer = null
-const waitLog = $ref([])
+const router = useRouter()
 
-const show = item => item != 0 && console.log(unpack(item))
-const startCombat = map => {
-  if (isCombat == true && combatTimer) return
-  isCombat = true
-  const correctMapMonsterData = unpack(
-    monsterDataTable.filter(
-      monster =>
-        monster.max - 2 <= player.level.current ||
-        monster.max <= player.level.current + 2
-    )
-  )
-
-  const { log, winner, loot } = combat(
-    player,
-    correctMapMonsterData,
-    map.maxMonsterCount
-  )
-
-  tempLog = log
-
-  combatTimer = setInterval(() => {
-    if (tempLog.length === 0) {
-      // 在此处处理战斗奖励和升级逻辑
-      // 战斗结果会返回战斗日志、奖励、胜者
-      // √ 每次战斗结束后会等待3S-5S，然后自动跳转到下一场战斗
-      clearInterval(combatTimer)
-      isCombat = false
-      combatLog = []
-      if (winner == 1) {
-        calcLoot(player, loot)
-      }
-      isWait = true
-
-      let count = 5
-      waitLog = []
-
-      waitTimer = setInterval(() => {
-        if (waitLog.length >= 5) {
-          clearInterval(waitTimer)
-          isWait = false
-          startCombat(map)
-        }
-
-        waitLog.push(`正在整顿，寻找下一个敌人...${count}s`)
-        count--
-      }, 1000)
-    }
-
-    combatLog.push(tempLog.shift())
-  }, 300)
-}
-
-onUnmounted(() => {
-  if (combatTimer) clearInterval(combatTimer)
-  if (waitTimer) clearInterval(waitTimer)
+const player = reactive({
+  name: '',
+  role: getData(900001),
+  race: getData(1000002)
 })
-</script>
-<style scoped>
-.lobby {
-  display: flex;
-  flex-direction: column;
+
+const loadPlayer = $ref({})
+
+const store = usePlayerStore()
+
+onMounted(() => {
+  loadPlayer = load()
+})
+
+const create = () => {
+  if (!player.name) return
+  const newPlayer = createNewPlayer(player.name, player.role.id, player.race.id)
+  player.name = ''
+  store.state = newPlayer
+  save(store.state)
+  router.push('/lobby')
 }
 
-.combat-wrapper {
-  width: 600px;
-  height: 300px;
-  border: 1px solid goldenrod;
-  overflow-y: scroll;
+const select = () => {
+  store.state = loadPlayer
+  router.push('/game')
 }
-</style>
+</script>
+<style scoped></style>
